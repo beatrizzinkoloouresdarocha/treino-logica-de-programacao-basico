75f0071cb5810f22143078643c9a03c35b53f069
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-05-05 11:01:32 -0300

    treino de variaveis de soma e divisao e menos concluidos estas.

---
+++ operacoes_matematicas_basicas.js
@@ -0,0 +1,33 @@
+
+
+// Funcao para soma de dois valores, funcoes sao trechos de codigos feitos para executar algo
+function soma() {
+    // declarando variaveis, variaveis sao utulizadas para armazenar valores
+    let primeiro_numero = 1
+    let segundo_numero = 2
+    let resultado_soma = primeiro_numero + segundo_numero
+    console.log('Resultado = ' + resultado_soma) //para escrever na tela de console
+}
+
+soma() //dessa forma que é chamada a execução de uma função nomedafuncao()
+
+function menos() {
+    let primeiro_numero = 50
+    let segundo_numero  = 10
+    let resultado_menos= primeiro_numero -segundo_numero
+    console.log('Resultado = ' + resultado_menos)
+}
+
+menos() 
+
+function divisao() {
+    let primeiro_numero = 50
+    let segundo_numero = 10
+    let resultado_divisao = primeiro_numero / segundo_numero
+    console.log('Resultado = '  + resultado_divisao)
+}
+
+divisao()
+
+//fazer o multiplicacao
+
