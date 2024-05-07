c9fd7ebe90e84da3d63343636329744439a46bba
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-05-07 17:34:55 -0300

    estou fazendo um teste .

--- Particularidades novo projeto/Js/script.js
+++ Particularidades novo projeto/Js/script.js
@@ -1,24 +1,24 @@
+// Imprime uma mensagem no console
 console.log("Primeiro");
 
+// Imprime outra mensagem no console
 console.log("Último");
 
+// Imprime mais uma mensagem no console
 console.log("Mais um console.log");
 
-// Declara uma variável
+// Declara uma variável e atribui um valor numérico
 var a = 1;
 
+// Altera o valor da variável para uma string
 a = 'teste';
 
+// Imprime o valor da variável no console
 console.log(a);
 
-// Este é um comentário
+// Este é um comentário de uma única linha
 
 /*
-  Este
-  é
-  um
-  comentário
-  de
-  múltiplas
-  linhas
+  Este é um comentário
+  de múltiplas linhas
 */
