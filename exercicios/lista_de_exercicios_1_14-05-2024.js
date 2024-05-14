51f561bb4798d1137b975def243a90a6af9508fb
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-05-14 11:21:25 -0300

    concluido o 4 exercicio e teste funcionou apareceu que o numero é par.

--- exercicios/lista_de_exercicios_1_14-05-2024.js
+++ exercicios/lista_de_exercicios_1_14-05-2024.js
@@ -33,5 +33,16 @@ let temperaturaCelsius =20 ; //temperatura em Celsius
 let temperaturaFahrenheit = (temperaturaCelsius * 9/5) + 32;
 console.log('A temperatura em Fahrenheit é: ' + temperaturaFahrenheit);
 
-//4) Verificação de número par ou ímpar:
+// 4) Verificação de número par ou ímpar:
 
+function verificarParidade(numero) {
+    if (numero % 2 === 0) {
+        return "Par";
+    } else {
+        return "Ímpar";
+    }
+}
+
+// Exemplo de uso
+let numero = 10;
+console.log(`O número ${numero} é ${verificarParidade(numero)}.`);
