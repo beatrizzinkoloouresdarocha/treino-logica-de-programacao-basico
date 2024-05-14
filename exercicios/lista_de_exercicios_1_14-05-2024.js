a45c8b7a3adb2a2527419f188631f7a94c23a61b
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-05-14 09:37:02 -0300

    concluido primeiro  exercicios correto.

---
+++ exercicios/lista_de_exercicios_1_14-05-2024.js
@@ -0,0 +1,16 @@
+//1) Calcule a soma de dois números:
+
+// Funcao para soma de dois valores, funcoes sao trechos de codigos feitos para executar algo
+function soma() {
+    // declarando variaveis, variaveis sao utulizadas para armazenar valores
+    let primeiro_numero = 100
+    let segundo_numero = 50
+    let resultado_soma = primeiro_numero + segundo_numero
+    console.log('Resultado = ' + resultado_soma) //para escrever na tela de console
+}
+
+soma() //dessa forma que é chamada a execução de uma função nomedafuncao()
+
+
+
+
