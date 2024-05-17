67465eea780d80c95c3196e5678d220747ae87f1
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-05-17 16:15:16 -0300

    Divisão de dois números:finalizei o exercicio 26 e testei e funcionou.

--- exercicios/lista_de_exercicios_1_14-05-2024.js
+++ exercicios/lista_de_exercicios_1_14-05-2024.js
@@ -378,3 +378,11 @@ multiplodetres()
 
 //26) Divisão de dois números:
 
+function divisao(){
+    let numero1=100;
+    let numero2=10;
+    let resultado_divisao=numero1/numero2
+    console.log('Resultado = '  + resultado_divisao)
+}
+
+divisao()
