8961119bcffcbd1a2147c690720a9eba3c5b9fbb
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-05-20 10:09:20 -0300

    concluido o //1) Verifique se um número é positivo e menor que 100.

---
+++ exercicios/lista_de_exercicios_2_20-05-2024.js
@@ -0,0 +1,7 @@
+//1) Verifique se um número é positivo e menor que 100.
+
+numero_exercicio_1=50;
+
+resultado_exercicio_1 = numero_exercicio_1 >=0 && numero_exercicio_1<100
+console.log('resultado exercicio 1: ' + resultado_exercicio_1) // retornou true ambas as condicoes sao verdadeiras
+
