e8b5c281c88b889aabc7280e649c05d69829993a
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-05-20 10:10:42 -0300

    comecei o exercicio 2 agora.

--- exercicios/lista_de_exercicios_2_20-05-2024.js
+++ exercicios/lista_de_exercicios_2_20-05-2024.js
@@ -5,3 +5,5 @@ numero_exercicio_1=50;
 resultado_exercicio_1 = numero_exercicio_1 >=0 && numero_exercicio_1<100
 console.log('resultado exercicio 1: ' + resultado_exercicio_1) // retornou true ambas as condicoes sao verdadeiras
 
+//2)Verifique se dois números são iguais ou a soma deles é maior que 100.
+
